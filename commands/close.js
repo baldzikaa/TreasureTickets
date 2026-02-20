53f743b79d53dff9b930ae5d7831e9b3d7d2740d
baldzika <buisnessgoingbeyond@gmail.com>
2026-02-19 19:00:17 -0500

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -7,16 +7,16 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('close')
         .setDescription('Close the current ticket')
-        .addStringOption(opt => opt.setName('reason').setDescription('Reason for closing the ticket').setRequired(false)),
+        .addStringOption(opt => opt.setName('reason').setDescription('Reason for closing the ticket!').setRequired(false)),
 
     async execute(interaction) {
         if (!hasPermission(interaction.member, 1)) {
-            return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return interaction.reply({ content: 'You do not have permission to use this command!', ephemeral: true });
         }
 
         const ticket = getTicketByChannel.get(interaction.channel.id);
         if (!ticket) {
-            return interaction.reply({ content: 'This command can only be used in a ticket channel.', ephemeral: true });
+            return interaction.reply({ content: 'This command can only be used in a ticket channel!', ephemeral: true });
         }
 
         const reason = interaction.options.getString('reason');
@@ -31,3 +31,4 @@ module.exports = {
         await performClose(interaction, interaction.user.id, reason);
     },
 };
+
