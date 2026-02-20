96f6fe7be971c2794ef61bc489a8fdb98e86dd98
baldzika <buisnessgoingbeyond@gmail.com>
2026-02-19 19:01:44 -0500

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -10,27 +10,28 @@ module.exports = {
 
     async execute(interaction) {
         if (!hasPermission(interaction.member, 3)) {
-            return interaction.reply({ content: 'You need Admin+ permission to use this command.', ephemeral: true });
+            return interaction.reply({ content: 'You need Admin+ permission to use this command!', ephemeral: true });
         }
 
         const ticket = getTicketByChannel.get(interaction.channel.id);
         if (!ticket) {
-            return interaction.reply({ content: 'This command can only be used in a ticket channel.', ephemeral: true });
+            return interaction.reply({ content: 'This command can only be used in a ticket channel!', ephemeral: true });
         }
 
         const user = interaction.options.getUser('user');
 
         if (user.id === ticket.creator_id) {
-            return interaction.reply({ content: 'You cannot remove the ticket creator.', ephemeral: true });
+            return interaction.reply({ content: 'You cannot remove the ticket creator!', ephemeral: true });
         }
 
         await interaction.channel.permissionOverwrites.delete(user.id);
 
         const embed = new EmbedBuilder()
-            .setDescription(`➖ <@${user.id}> has been removed from this ticket by <@${interaction.user.id}>.`)
+            .setDescription(`➖ <@${user.id}> has been removed from this ticket by <@${interaction.user.id}>!`)
             .setColor(0xED4245)
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
     },
 };
+
